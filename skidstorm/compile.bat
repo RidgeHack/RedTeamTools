0a91fd897497687abe141463d8101932a166aef3
RidgeHack <113032514+RidgeHack@users.noreply.github.com>
2025-07-04 10:38:25 +0100

    Delete skidstorm directory

--- skidstorm/compile.bat
+++
@@ -1,2 +0,0 @@
-cl.exe /c /nologo /Od /Ob0 /Oy- /MT /W0 /GS- /Tp obfuscated_shellcoderunner.c /link /OUT:obfuscated_shellcoderunner.exe /SUBSYSTEM:CONSOLE 
-link.exe /FORCE obfuscated_shellcoderunner.obj
